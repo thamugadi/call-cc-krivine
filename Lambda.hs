dd30ca54dbc5bb21a4a67621ccd082bf14700760
aramya <22577625+thamugadi@users.noreply.github.com>
2023-01-01 21:47:39 +0100

    Create Lambda.hs

---
+++ Lambda.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE GADTs #-}
+module Lambda where
+
+data Type where
+  Base   :: String -> Type
+  App    :: Type -> Type -> Type deriving (Eq, Show)
+data Term where
+  Var    :: String -> Term
+  Lambda :: Term -> Type -> Term -> Term
+  Apply  :: Term -> Term -> Term 
+  CC     :: Term
+  Cont   :: [Term] -> Term deriving (Eq, Show)
+
+beta :: Term -> Term -> Term -> Term 
+beta a b (Var t) 
+  | (Var t) == a = b
+  | otherwise = Var t
+beta a b (Lambda x ty t) = Lambda x ty (beta a b t)
+beta a b (Apply t u) = Apply (beta a b t) (beta a b u)
+
+initType :: Type -> Type
+initType (App t1 t2) =
+  case t2 of
+    (Base _) -> t1  
+    _        -> App t1 (initType t2) 
+
+composeTypeCheck :: Type -> Type -> Bool 
+composeTypeCheck (Base a) _ = False
+composeTypeCheck a b
+  | initType a == b = True
+  | otherwise = False
