96502a9237d5141d7204141a89ed13930dbe11b8
thamugadi <qirimhangli@gmail.com>
2023-01-04 15:54:10 +0100

    initType :: Type -> Maybe Type

--- Lambda.hs
+++ Lambda.hs
@@ -18,16 +18,12 @@ beta a b (Var t)
 beta a b (Lambda x ty t) = Lambda x ty $ beta a b t
 beta a b (Apply t u) = Apply (beta a b t) (beta a b u)
 
-initType :: Type -> Type
-initType (Base _) = error "Base type."
-initType (App t1 t2) =
-  case t2 of
-    (Base _) -> t1  
-    _        -> App t1 (initType t2) 
+initType :: Type -> Maybe Type
+initType (Base _) = Nothing 
+initType (App t1 (Base _)) = Just t1
+initType (App t1 t2) = (initType t2) >>= (\x -> Just $ App t1 x)
 
-tailType :: Type -> Type
-tailType (Base _) = error "Base type."
-tailType (App t1 t2) =
-  case t1 of
-    (Base _) -> t2
-    _        -> App (tailType t1) t2
+tailType :: Type -> Maybe Type
+tailType (Base _) = Nothing 
+tailType (App (Base _) t2) = Just t2
+tailType (App t1 t2) = (tailType t1) >>= (\x -> Just $ App x t2)
