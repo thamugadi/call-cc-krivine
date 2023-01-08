ba37d51eed1a70921014316cc25433aae29f3815
thamugadi <qirimhangli@gmail.com>
2023-01-08 16:49:59 +0100

    More concise.

--- Krivine.hs
+++ Krivine.hs
@@ -3,17 +3,13 @@ import Lambda
 --Typechecking to be implemented.
 
 krivine1 :: (Term, [Term]) -> Either String (Term, [Term])
-krivine1 (Var x, t) = Right (Var x, t)
 
-krivine1 (Lambda x t, []) = Right (Lambda x t, [])
 krivine1 (Lambda x t, (s:stack)) = Right (beta x s t, stack)
-
 krivine1 (Apply CC f, stack) = Right (f, (Cont stack):stack)
 krivine1 (Cont stack1, s:_) = Right (s, stack1)
-
 krivine1 (Apply a b, stack) = Right (a, b:stack)
 
-krivine1 _ = Left "Error"
+krivine1 a = Right a
 
 krivine_ :: (Term, [Term]) -> [Either String (Term, [Term])]
 krivine_ a
