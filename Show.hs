54304b0e689c8fb6cca54b9c12719113aaf0431b
aramya <22577625+thamugadi@users.noreply.github.com>
2023-01-13 02:48:28 +0100

    Showing clean terms.

--- Show.hs
+++ Show.hs
@@ -2,6 +2,9 @@ module Show where
 import Lambda
 import Data.List (intercalate)
 
+rnas :: String -> String
+rnas s = [x | x <- s, x /= ';' && ((not (elem x ['0'..'9'])) || x == ' ')]
+
 t2str :: Term -> String
 t2str (Var a) = a
 t2str (Lambda x e) = "(λ"++(t2str x)++"."++(t2str e)++")"
@@ -12,8 +15,12 @@ t2str (Cont c) = "continuation " ++ show c
 showKrivine :: Either String [(Term, [Term])] -> String
 showKrivine (Left s) = s++"\n"
 showKrivine (Right []) = ""
+showKrivine (Right [(t@(Var x), s@([a]))]) =
+    (t2str t)++" *** "++
+    "["++
+    intercalate ", " (map t2str s)++"]"++ " not evaluated!\n"
 showKrivine (Right ((t,s):kr)) =
   (t2str t)++" *** "++
   "["++
   intercalate ", " (map t2str s)++"]\n"++
-  (showKrivine (Right kr))    
+  (showKrivine (Right kr))
