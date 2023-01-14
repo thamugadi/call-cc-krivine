daf36bfba7a7ae37d92ad318b9390ef17e6b498c
aramya <22577625+thamugadi@users.noreply.github.com>
2023-01-14 17:51:20 +0100

    more readable

--- Lambda.hs
+++ Lambda.hs
@@ -11,10 +11,10 @@ data Term where
 alpha1 :: Int -> [(String, Int)] -> Term -> Term
 alpha1 n bound (Var t)
   | p /= Nothing = Var $ t ++ ";" ++ (show $ snd $ (\(Just a) -> a) p)
-  | otherwise = Var t where p = (find (\(s,i) -> s == t) bound)
+  | otherwise    = Var t where p = (find (\(s,i) -> s == t) bound)
 alpha1 n bound (Lambda (Var x) t) = Lambda (Var $ x++";"++(show n)) (alpha1 (n+1) ((x,n):bound) t)
-alpha1 n bound (App a b) = App (alpha1 n bound a) (alpha1 n bound b)
-alpha1 _ _ a = a
+alpha1 n bound (App a b)          = App (alpha1 n bound a) (alpha1 n bound b)
+alpha1 _ _ a                      = a
 
 alpha :: Term -> Term
 alpha = alpha1 0 []
@@ -22,11 +22,11 @@ alpha = alpha1 0 []
 beta1 :: Term -> Term -> Term -> Term 
 beta1 a b (Var t) 
   | (Var t) == a = b
-  | otherwise = Var t
+  | otherwise    = Var t
 beta1 a b (Lambda x t) = Lambda x $ beta1 a b t
-beta1 a b (App t u) = App (beta1 a b t) (beta1 a b u)
-beta1 _ b _ = b
+beta1 a b (App t u)    = App (beta1 a b t) (beta1 a b u)
+beta1 _ b _            = b
 
 beta :: Term -> Term -> Term
 beta (Lambda x t) s = beta1 x s t
-beta a _ = a
+beta a _            = a
