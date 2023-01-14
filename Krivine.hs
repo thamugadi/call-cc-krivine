9bd3bec3e80d0052c6ce2200f5bf7c56269ed2ef
thamugadi <qirimhangli@gmail.com>
2023-01-14 17:19:23 +0100

    concise

--- Krivine.hs
+++ Krivine.hs
@@ -14,7 +14,8 @@ krivine1 a = a
 krivine2 :: (Term, [Term]) -> [(Term, [Term])]
 krivine2 a
    | krivine1 a == a = [a]
-   | otherwise = krivine1 a : (krivine2 $ krivine1 a)
+   | otherwise = eval : krivine2 eval 
+       where eval = krivine1 a
 
 krivine3 :: Term -> [(Term, [Term])]
 krivine3 a = (alphaeq, []) : krivine2 (alphaeq, [])
