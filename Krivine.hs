7430144347f13d86cf3d6a65574e1d463dd5fe4c
aramya <22577625+thamugadi@users.noreply.github.com>
2023-01-14 16:57:10 +0100

    complete display.

--- Krivine.hs
+++ Krivine.hs
@@ -1,25 +1,25 @@
 module Krivine where
 import Lambda
 
-krivine1 :: (Term, [Term]) -> Either String (Term, [Term])
+krivine1 :: (Term, [Term]) -> (Term, [Term])
 
-krivine1 (App CC f, stack) = Right (f, (Cont stack):stack)
-krivine1 (Cont stack1, s:_) = Right (s, stack1)
+krivine1 (App CC f, stack) = (f, (Cont stack):stack)
+krivine1 (Cont stack1, s:_) = (s, stack1)
 
-krivine1 (t@(Lambda _ _), (s:stack)) = Right (beta t s, stack)
-krivine1 (App a b, stack) = Right (a, b:stack)
+krivine1 (t@(Lambda _ _), (s:stack)) = (beta t s, stack)
+krivine1 (App a b, stack) = (a, b:stack)
 
-krivine1 a = Right a
+krivine1 a = a
 
-krivine2 :: (Term, [Term]) -> [Either String (Term, [Term])]
+krivine2 :: (Term, [Term]) -> [(Term, [Term])]
 krivine2 a
-  | (krivine1 a) == Right a = pure $ Right a
-  | otherwise = krivine1 a : [(krivine1 a) >>= (\x -> last $ krivine2 x)]
+   | krivine1 a == a = [a]
+   | otherwise = krivine1 a : krivine2 (krivine1 a)
 
-krivine3 :: Term -> Either String [(Term, [Term])]
-krivine3 a = sequence $ (Right (alphaeq, [])) : krivine2 (alphaeq, [])
+krivine3 :: Term -> [(Term, [Term])]
+krivine3 a = (alphaeq, []) : krivine2 (alphaeq, [])
   where alphaeq = alpha a
 
 krivine :: Either a Term -> Either String [(Term, [Term])]
-krivine (Left _) = Left "Parsing Error"
-krivine (Right k) = krivine3 k
+krivine (Left _)  = Left "Parsing Error"
+krivine (Right k) = Right $ krivine3 k
