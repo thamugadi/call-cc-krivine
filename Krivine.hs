a96148234ae49195dfba8dff67039c3b91945011
aramya <22577625+thamugadi@users.noreply.github.com>
2023-01-18 16:03:18 +0100

    no longer redundant

--- Krivine.hs
+++ Krivine.hs
@@ -13,7 +13,7 @@ krivine1 a = a
 
 krivine2 :: (Term, [Term]) -> [(Term, [Term])]
 krivine2 a
-   | eval == a = [a]
+   | eval == a = []
    | otherwise = eval : krivine2 eval 
        where eval = krivine1 a
 
