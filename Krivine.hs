f0d7df3bc7b643d16fedd881a71e034e2c14c90d
thamugadi <qirimhangli@gmail.com>
2023-01-24 19:29:34 +0100

    Better handling of call/cc

--- Krivine.hs
+++ Krivine.hs
@@ -1,25 +1,29 @@
 module Krivine where
 import Lambda
 
-krivine1 :: (Term, [Term]) -> (Term, [Term])
+krivine1 :: (Term, [Term], Integer) -> (Term, [Term], Integer)
 
-krivine1 (App CC f, stack)  = (f, (Cont stack):stack)
-krivine1 (Cont stack1, s:_) = (s, stack1)
+--krivine1 (App CC f, stack, n)  = (f, (Cont stack):stack, n+1)
+krivine1 (CC, (f:stack), n) = (f, (Cont stack):stack, n+1)
+krivine1 (Cont stack1, s:_, n) = (s, stack1, n+1)
 
-krivine1 (t@(Lambda _ _), (s:stack)) = (beta t s, stack)
-krivine1 (App a b, stack)            = (a, b:stack)
+krivine1 (t@(Lambda _ _), (s:stack), n) = (beta t s, stack, n+1)
+krivine1 (App a b, stack, n)            = (a, b:stack, n+1)
+
+krivine1 (Clock, (s:stack), n) = (s, (Instr (n+1):stack), n+1)
 
 krivine1 a = a
 
-krivine2 :: (Term, [Term]) -> [(Term, [Term])]
+krivine2 :: (Term, [Term], Integer) -> [(Term, [Term], Integer)]
 krivine2 a
    | eval == a = []
    | otherwise = eval : krivine2 eval 
        where eval = krivine1 a
 
 krivine3 :: Term -> [(Term, [Term])]
-krivine3 a = (alphaeq, []) : krivine2 (alphaeq, [])
+krivine3 a = (alphaeq, []) : map pair (krivine2 (alphaeq, [], 0))
   where alphaeq = alpha a
+        pair = \(a,b,c) -> (a,b)
 
 krivine :: Either a Term -> Either String [(Term, [Term])]
 krivine (Left _)  = Left "Parsing Error"
