b61587f3f131179e63fdbcdc5ba186ac362ff46f
aramya <22577625+thamugadi@users.noreply.github.com>
2023-02-28 12:15:40 +0100

    REPL

--- Krivine.hs
+++ Krivine.hs
@@ -15,6 +15,7 @@ krivine1 ((t@(Lambda _ _), (s:stack), n),c) = ((beta t s, stack, n+1),c)
 krivine1 ((App a b, stack, n),c)            = ((a, b:stack, n+1),c)
 
 krivine1 ((Var "define", (Var a:b:x:s), n), c) = ((x, s, n+1), (a, b):c)
+krivine1 ((v@(Var "define"), (Var a:b:[]), n), c) = ((v, [], n+1), (a, b):c)
 
 krivine1 ((t@(Var a), stack, n), context) = 
   ((maybe t snd $ find ((==a) . fst) context, stack, n), context)
--- Main.hs
+++ Main.hs
@@ -1,11 +1,3 @@
-import System.Environment (getArgs)
-import Show (showKrivine)
-import Krivine 
-import Data.Maybe (listToMaybe)
+import REPL
 
-main :: IO ()
-main = do
-  args <- getArgs
-  case listToMaybe args of
-    Nothing -> putStrLn "Usage: runghc Main.hs \"<expression>\""
-    Just a  -> putStr $ showKrivine $ runKrivine a []
+main = repl [] 
