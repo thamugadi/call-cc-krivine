ef62ce6233f6e55d2bcf04bf281b8b52889d71d5
thamugadi <qirimhangli@gmail.com>
2023-02-28 12:15:40 +0100

    REPL

---
+++ REPL.hs
@@ -0,0 +1,22 @@
+module REPL where
+
+import Krivine
+import Show
+import Data.Maybe (listToMaybe)
+import System.Environment (getArgs)
+
+cmd :: IO ()
+cmd = do  
+  args <- getArgs  
+  case listToMaybe args of  
+    Nothing -> putStrLn "Usage: runghc Main.hs \"<expression>\""  
+    Just a  -> putStr $ showKrivine $ runKrivine a []  
+
+repl :: Context -> IO ()
+repl context = do
+  x <- getLine
+  let k = runKrivine x context
+  putStr $ showKrivine k
+  case k of
+    Left b  -> return () 
+    Right a -> repl $ snd $ last a 
