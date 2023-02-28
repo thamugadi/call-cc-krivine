0fdf2ec2a4ffe9ebadd2b32dfb2cefe5df5ec3b6
thamugadi <qirimhangli@gmail.com>
2023-02-28 21:57:21 +0100

    supports REPL and argv

--- Main.hs
+++ Main.hs
@@ -1,3 +1,10 @@
 import Interpreter 
+import Data.Maybe (listToMaybe)
+import System.Environment (getArgs)
 
-main = repl [] 
+main :: IO ()
+main = do
+  args <- getArgs
+  case listToMaybe args of
+    Nothing -> repl []
+    Just a  -> cmd args 
